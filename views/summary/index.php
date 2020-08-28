221976d810584434901d0fba560cd8826bacc517
roydeanjr <36243458+roydeanjr@users.noreply.github.com>
2020-08-28 11:30:29 -0500

    Expand width of location list
    Expand for accommodating long location names. Easier to read and shorter list if not wrapped.

--- views/summary/index.php
+++ views/summary/index.php
@@ -15,7 +15,7 @@
 		    	
 	        <div class="d-sm-flex flex-sm-wrap w-100 no-gutters">
 		        
-				<div class="col col-sm-2 mr-3">
+				<div class="col col-sm-3 mr-3">
 					
 					<div class="filter-scope date form-group">
 						
@@ -29,7 +29,7 @@
             		
 		        </div>	       
 		        		        
-				<div class="col col-sm-2 mr-3">
+				<div class="col col-sm-3 mr-3">
 					
 					<button type="submit" class="btn btn-primary">View</button>
 					
