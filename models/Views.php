4a8f1e49fc75fb1222d2e34ba0c6cedcbe0608fd
BreakSecurity <38836022+BreakSecurity@users.noreply.github.com>
2020-12-14 20:21:20 +0100

    Name is now required

--- models/Views.php
+++ models/Views.php
@@ -27,71 +27,75 @@ class Views extends Model
         'categories' => 'array',
         'display' => 'serialize',
     ];
-    
+
     public $rules = [
+        'name' => 'required',
         'locations' => 'required',
         'display.order_count' => 'required|int|min:0',
         'display.refresh_interval' => 'required|int|min:10',
     ];
-    
+
     public function beforeSave()
     {
+        if (!Request::input('View.name'))
+            $this->name = [];
+
         if (!Request::input('View.locations'))
             $this->locations = [];
-            
+
         if (!Request::input('View.categories'))
             $this->categories = [];
     }
-        
+
     public static function getCardLineOptions($line)
     {
-        
+
         switch ($line)
         {
             case 1:
-                
+
                 return [
                     0 => lang('lang:thoughtco.kitchendisplay::default.option_blank'),
                     1 => lang('lang:thoughtco.kitchendisplay::default.option_name_id'),
                     2 => lang('lang:thoughtco.kitchendisplay::default.option_id_name'),
                 ];
-            
-            break;   
-            
+
+            break;
+
             case 2:
-                
+
                 return [
                     0 => lang('lang:thoughtco.kitchendisplay::default.option_blank'),
                     1 => lang('lang:thoughtco.kitchendisplay::default.option_phone_time_total'),
                     2 => lang('lang:thoughtco.kitchendisplay::default.option_time_phone_total'),
                 ];
-            
-            break;             
-             
+
+            break;
+
             case 3:
-                
+
                 return [
                     0 => lang('lang:thoughtco.kitchendisplay::default.option_blank'),
                     1 => lang('lang:thoughtco.kitchendisplay::default.option_address'),
                 ];
-            
-            break;             
+
+            break;
         }
-        
+
         return [];
     }
-    
+
     public static function getCategoriesOptions()
     {
 	    return Categories_model::all()->pluck('name', 'category_id');
     }
-    
+
     public static function getLocationsOptions()
     {
 	    $locations = [];
 	    foreach (Locations_model::isEnabled()->get() as $location){
 	    	$locations[$location->location_id] = $location->location_name;
-	    };	  
-	    return collect($locations);  
-    }   
+	    };
+	    return collect($locations);
+    }
 }
