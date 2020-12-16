db4f987dd10a4329bd9698562d928ac4058b3e11
BreakSecurity <38836022+BreakSecurity@users.noreply.github.com>
2020-12-16 16:20:14 +0100

    Revert "Update Views.php"
    This reverts commit 37eba3f0128bd686cecf2180330838b5769ed68a.

--- models/Views.php
+++ models/Views.php
@@ -44,6 +44,7 @@ class Views extends Model
             $this->categories = [];
     }
 
+>>>>>>> parent of d042886... Revert "Merge remote-tracking branch 'upstream/master'"
     public static function getCardLineOptions($line)
     {
 
